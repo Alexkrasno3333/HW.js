2fc86cd481479d98daf9073cece5443ca8f1174f
username <oleksandrkrasnokutskyi@MacBook-Air-Oleksandr.local>
2023-04-17 19:35:35 +0300

    hw 25

--- mymain.js
+++ mymain.js
@@ -1,111 +1,89 @@
 //hw 25
 class UserService {
-  static #baseUrl = "https://dummyjson.com/users";
-  static getAllUsers = () => {
-    fetch(this.#baseUrl)
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  };
-  static getUser = (id) => {
-    fetch(`${this.#baseUrl}/${id}`)
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  };
-  static searchUsers = (query) => {
-    fetch(`${this.#baseUrl}/search?q=${query}`)
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  };
-  static filterUsers = async (key, value) => {
-    try {
-      const url = new URL(`${this.#baseUrl}/filter`);
-      url.searchParams.set("key", key);
-      url.searchParams.set("value", value);
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  static paginateUsers = async (limit, skip) => {
-    try {
-      const url = new URL(this.#baseUrl);
-      url.searchParams.set("limit", limit);
-      url.searchParams.set("skip", skip);
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  static getUserCart = async (id) => {
-    try {
-      const res = await fetch(`${this.#baseUrl}/${id}/carts`);
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  static getUserPosts = async (id) => {
-    try {
-      const res = await fetch(`${this.#baseUrl}/${id}/posts`);
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  static getUserTodos = async (id) => {
-    try {
-      const res = await fetch(`${this.#baseUrl}/${id}/todos`);
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  static addUser = async (body) => {
-    try {
-      const res = await fetch(`${this.#baseUrl}/add`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  static updateUser = async (id, body) => {
-    try {
-      const res = await fetch(`${this.#baseUrl}/${id}`, {
-        method: "PUT" /* or PATCH */,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  static deleteUser = async (id) => {
-    try {
-      const res = await fetch(`${this.#baseUrl}/${id}`, {
-        method: "DELETE",
-      });
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-}
+			static async getUsers() {
+				try {
+					const response = await fetch('https://dummyusersapi.herokuapp.com/api/users');
+					const data = await response.json();
+					console.log(data);
+				} catch(error) {
+					console.error(error);
+				}
+			}
 
+			static async getUserById(id) {
+				try {
+					const response = await fetch(`https://dummyusersapi.herokuapp.com/api/users/${id}`);
+					const data = await response.json();
+					console.log(data);
+				} catch(error) {
+					console.error(error);
+				}
+			}
+
+			static async createUser(user) {
+				try {
+					const response = await fetch('https://dummyusersapi.herokuapp.com/api/users', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify(user)
+					});
+					const data = await response.json();
+					console.log(data);
+				} catch(error) {
+					console.error(error);
+				}
+			}
+
+			static async updateUser(id, user) {
+				try {
+					const response = await fetch(`https://dummyusersapi.herokuapp.com/api/users/${id}`, {
+						method: 'PUT',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify(user)
+					});
+					const data = await response.json();
+					console.log(data);
+				} catch(error) {
+					console.error(error);
+				}
+			}
+
+			static async deleteUser(id) {
+				try {
+					const response = await fetch(`https://dummyusersapi.herokuapp.com/api/users/${id}`, {
+						method: 'DELETE'
+					});
+					const data = await response.json();
+					console.log(data);
+				} catch(error) {
+					console.error(error);
+				}
+			}
+
+			static async deleteUsers() {
+				try {
+					const ids = [1, 2, 3, 4];
+					const randomId = Math.floor(Math.random() * (10000 - 5 + 1)) + 5;
+					ids.push(randomId);
+					const requests = ids.map(id => fetch(`https://dummyusersapi.herokuapp.com/api/users/${id}`, {
+						method: 'DELETE'
+					}));
+					const responses = await Promise.allSettled(requests);
+					console.log(responses);
+				} catch(error) {
+					console.error(error);
+				}
+			}
+		}
+
+		// Examples
+		UserService.getUsers();
+		UserService.getUserById(1);
+		UserService.createUser({ name: 'John Doe', email: 'johndoe@example.com', password: 'password' });
+		UserService.updateUser(1, { name: 'Jane Doe', email: 'janedoe@example.com', password: 'password' });
+		UserService.deleteUser(1);
+		UserService.deleteUsers();
